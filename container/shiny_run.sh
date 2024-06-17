431f402b46d855d3b23ef3b0513bada0e3a69723
mmerono <104634550+mmerono@users.noreply.github.com>
2024-06-17 23:46:40 +0200

    Add files via upload

---
+++ container/shiny_run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=shinyApp
+#SBATCH --output=%x.out
+#SBATCH --error=%x.err
+#SBATCH --time=12:00:00
+#SBATCH --mem=110G
+
+
+singularity exec \
+  --bind /ijc/LABS/MERKEL/DATA/PROJECTS/mmerono/:/scRNAseq_data \
+  scRNAseq_refData.sif \
+  Rscript -e "shiny::runApp('/scRNAseq_data/singularity/app.R')"
